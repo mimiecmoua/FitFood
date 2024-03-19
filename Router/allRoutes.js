191d299961aab9129019009dc8e1329f14ffbcfa
Emilie <dionatsumi@gmail.com>
2024-03-19 11:35:38 +0100

    Add modal edition/suppression galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "la galerie", "/pages/galerie.html"),
     new Route("/menu", "le menu", "/pages/menu.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
